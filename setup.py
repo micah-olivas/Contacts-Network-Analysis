2f79b17e22731638853a70344b4ed958acae6afb
micah-olivas <51717162+micah-olivas@users.noreply.github.com>
2021-08-17 15:48:56 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+	name = 'contacts_network_analysis',
+	version = '0.1.0',
+	url = 'https://github.com/micah-olivas/Contacts-Network-Analysis.git',
+	author = 'Micah Olivas',
+	author_email = '',
+	description = 'Contacts Network Analysis',
+	packages = find_packages(),    
+	install_requires = requirements,
